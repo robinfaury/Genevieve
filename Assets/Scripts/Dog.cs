086a5f9c6f220a2deaca8a2025bad8b0c3e9f52d
Yannis <yannisguyon@hotmail.com>
2015-11-08 10:42:44 +0100

    wsfhs

---
+++ Assets/Scripts/Dog.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dog : MonoBehaviour
+{
+    private float rotatingSpeed = 0.2f;
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        transform.rotation *= Quaternion.AngleAxis(rotatingSpeed * Time.deltaTime, Vector3.up);
+    }
+    public void Rotate()
+    {
+        rotatingSpeed = 2.5f * 360.0f;
+    }
+}
