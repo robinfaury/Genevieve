7c84a6f55aa8cd455322b000134ed0b408442af2
Celia <celiaporcher@free.fr>
2015-11-08 07:08:20 +0100

    tromblooon + particles

---
+++ Assets/Scripts/InteractableTromblon.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteractableTromblon : Interactable {
+
+    private Timer timer;
+    private const float hitDuration = 0.333f;
+    public override void Init()
+    {
+        timer = new Timer();
+    }
+
+    public override void Take(Genevieve genevieve)
+    {
+        base.Take(genevieve);
+        timer.Reset();
+    }
+    public override void Held(Genevieve genevieve)
+    {
+        transform.position = genevieve.leftHand.position;
+        transform.rotation = Quaternion.LookRotation(genevieve.rightHand.position - genevieve.leftHand.position);
+        if (genevieve.gameManager.running && Input.GetMouseButton(1) && !genevieve.moving)
+        {
+            genevieve.animToPlay = 3;
+            if (timer.Get() >= hitDuration)
+            {
+                timer.Substract(hitDuration);
+                genevieve.gameManager.IncreaseCurrentLevelProgress(0);
+            }
+        }
+        else
+        {
+            timer.Reset();
+        }
+    }
+}
--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -33,7 +33,6 @@ public class Main : MonoBehaviour
 
     void Update()
     {
-
         if(Input.GetKeyDown(KeyCode.Escape))
         {
             pauseMenu = !pauseMenu;
